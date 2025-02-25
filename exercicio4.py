3bc9913b818a7f583b6a438fc8fa25ba8db0e468
PedroColombo2003 <pedrocorrea087@gmail.com>
2025-02-25 20:30:21 -0300

    Add files via upload

---
+++ exercicio4.py
@@ -0,0 +1,28 @@
+
+"""
+Exercicio 4
+Autor: Pedro Colombo
+Data: 25/02/2025
+Versão: 0.0.1
+Problema: 
+
+pergunte o nome do usuario e apresente a mensagem Oi,... , seguida pelo nome do usuario
+
+"""
+
+
+
+
+# Alocação de memória
+
+
+# Entrada de dados
+
+nome = input("Qual o seu nome? ")
+
+# Processamento de dados
+
+
+#Saida de dados
+
+print(f"Oi, {nome}")
